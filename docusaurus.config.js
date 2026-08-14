7124490cc2a4df94a4b101133b1ab993cdc8b80c
Pumelela <pumelelamapukata1@gmail.com>
2026-08-14 22:53:36 +0200

    Update baseUrl for GitHub Pages deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ tagline: 'Project Documentation',
   url: 'https://pumelela1.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/Adamas2Aurum-Documentation/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
