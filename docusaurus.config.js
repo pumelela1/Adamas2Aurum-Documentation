0ee47bc783556f0bc4bb148cf25301209f304d5d
Pumelela <pumelelamapukata1@gmail.com>
2026-08-16 10:36:50 +0200

    Refactor Docusaurus config for clarity and updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,42 +1,31 @@
 // @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
 
 import {themes as prismThemes} from 'prism-react-renderer';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-title: 'Adamas2Aurum',
-tagline: 'Project Documentation',
+  title: 'Adamas2Aurum',
+  tagline: 'Project Documentation',
   favicon: 'img/favicon.ico',
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    v4: true,
   },
 
-  // Set the production url of your site here
+  // GitHub Pages
   url: 'https://pumelela1.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/Adamas2Aurum-Documentation/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'pumelela1', // Usually your GitHub org/user name.
-  projectName: 'Adamas2Aurum-Documentation', // Usually your repo name.
+  // GitHub repository
+  organizationName: 'pumelela1',
+  projectName: 'Adamas2Aurum-Documentation',
   deploymentBranch: 'gh-pages',
-trailingSlash: false,
 
-  onBrokenLinks: 'warn',
+  trailingSlash: false,
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
+  // Fail the build if we accidentally create broken links.
+  onBrokenLinks: 'throw',
+
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -49,26 +38,16 @@ trailingSlash: false,
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
+
+          // Allows GitHub's "Edit this page" link to point
+          // directly to the documentation source files.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/pumelela1/Adamas2Aurum-Documentation/tree/main/',
         },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
+
+        // We are using Docusaurus as documentation, not as a blog.
+        blog: false,
+
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -79,17 +58,19 @@ trailingSlash: false,
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
+
       colorMode: {
         respectPrefersColorScheme: true,
       },
+
       navbar: {
-  title: 'Adamas2Aurum',
+        title: 'Adamas2Aurum',
         logo: {
           alt: 'Adamas2Aurum Logo',
           src: 'img/logo.svg',
         },
+
         items: [
           {
             type: 'docSidebar',
@@ -97,54 +78,70 @@ trailingSlash: false,
             position: 'left',
             label: 'Documentation',
           },
-          
+
+          {
+            href: 'https://github.com/pumelela1/Adamas2Aurum-Documentation',
+            label: 'GitHub',
+            position: 'right',
+          },
         ],
       },
+
       footer: {
         style: 'dark',
+
         links: [
           {
-            title: 'Docs',
+            title: 'Documentation',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Project Overview',
+                to: '/docs/project-overview/overview',
+              },
+              {
+                label: 'Project Management',
+                to: '/docs/project-management/overview',
+              },
+              {
+                label: 'Requirements & Design',
+                to: '/docs/design/requirements',
               },
             ],
           },
+
           {
-            title: 'Community',
+            title: 'Project',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: 'Implementation',
+                to: '/docs/implementation/overview',
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                label: 'Testing',
+                to: '/docs/testing/test-plan',
               },
               {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
+                label: 'Deployment',
+                to: '/docs/deployment/overview',
               },
             ],
           },
+
           {
-            title: 'More',
+            title: 'Repository',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/pumelela1/Adamas2Aurum-Documentation',
               },
             ],
           },
         ],
-        copyright: `Adamas2Aurum Project Documentation © ${new Date().getFullYear()}`,
+
+        copyright:
+          `Adamas2Aurum Project Documentation © ${new Date().getFullYear()}`,
       },
+
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
